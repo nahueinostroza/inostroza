52b9083ca9065ffa2ae611b34fa3a6ded31693d6
nahuelinostroza <nahuelon8@hotmail.com>
2022-08-22 17:28:10 -0300

    agregue carpeta componets, instale tailiwind y agregue navbar al proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1 className='text-blue-700'>XenEsport</h1>
+    </div>
+  );
+}
+
+export default App;
